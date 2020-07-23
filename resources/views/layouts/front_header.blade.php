30491a301959983cdb23294709728fb3c242eafb
Andrew Ben Richard <andrewbenrichard@gmail.com>
2020-07-23 16:22:34 +0100

    public

--- resources/views/layouts/front_header.blade.php
+++ resources/views/layouts/front_header.blade.php
@@ -1,29 +1,29 @@
 
  {{-- vue styles--}}
- <link href="{{ asset('css/front_app.css') }}" rel="stylesheet">
+ <link href="{{ asset('public/css/front_app.css') }}" rel="stylesheet">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="public/assets/css/bootstrap.min.css">
         <!-- Owl Carousel CSS -->
-        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="public/assets/css/owl.carousel.min.css">
         <!-- Theme default CSS -->
-        <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="public/assets/css/owl.theme.default.min.css">
         <!-- Slick Slider CSS -->
-        <link rel="stylesheet" href="assets/css/slick.css">
+        <link rel="stylesheet" href="public/assets/css/slick.css">
         <!-- Slick Theme CSS -->
-        <link rel="stylesheet" href="assets/css/slick-theme.css">
+        <link rel="stylesheet" href="public/assets/css/slick-theme.css">
         <!-- Icofont CSS -->
-        <link rel="stylesheet" href="assets/css/icofont.min.css">
+        <link rel="stylesheet" href="public/assets/css/icofont.min.css">
         <!-- Magnific POpup CSS -->
-        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="public/assets/css/magnific-popup.css">
         <!-- Flaticon CSS -->
-        <link rel="stylesheet" href="assets/css/flaticon.css">
+        <link rel="stylesheet" href="public/assets/css/flaticon.css">
         <!-- Meanmenu CSS -->
-        <link rel="stylesheet" href="assets/css/meanmenu.css">
+        <link rel="stylesheet" href="public/assets/css/meanmenu.css">
         <!-- Style CSS -->
-        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="public/assets/css/style.css">
         <!-- Responsive CSS -->
-        <link rel="stylesheet" href="assets/css/responsive.css">
+        <link rel="stylesheet" href="public/assets/css/responsive.css">
 
 
  <!-- CSRF Token -->
